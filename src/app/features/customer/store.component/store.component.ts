2896521e36efac784c5c4bfa9064cb93532aae8a
Castropy <randrescastroi@gmail.com>
2026-03-29 01:42:31 -0400

    Feat:(components) components generated for config the route

---
+++ src/app/features/customer/store.component/store.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store.component',
+  imports: [],
+  templateUrl: './store.component.html',
+  styles: ``,
+})
+export class StoreComponent {
+
+}
