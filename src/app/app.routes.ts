f4b84ae68117ccbdf0bceba2706269472ce4b5f2
Castropy <randrescastroi@gmail.com>
2026-03-30 18:02:03 -0400

    routing: set LobbyComponent as root path and update navigation fallback

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,46 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/guards/auth-guard'; // Importamos tu portero
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
+  // 1. EL LOBBY: La puerta de entrada (Pública)
+  {
+    path: '',
+    loadComponent: () => import('./features/lobby/lobby.component').then(m => m.LobbyComponent)
+  },
+  
+  // 2. AUTH: Login y Registro
   {
     path: 'auth',
     loadComponent: () => import('./features/auth/auth.component/auth.component').then(m => m.AuthComponent)
   },
+
+  // 3. STORE: Exploración de productos (Pública)
   {
     path: 'store',
     loadComponent: () => import('./features/customer/store.component/store.component').then(m => m.StoreComponent)
   },
-  // --- RUTAS PROTEGIDAS ---
+
+  // --- RUTAS PROTEGIDAS (Requieren Login) ---
+  
+  // DASHBOARD: Para los dueños de negocios
   {
     path: 'dashboard',
     loadComponent: () => import('./features/business/dashboard.component/dashboard.component').then(m => m.DashboardComponent),
-    canActivate: [authGuard] // Solo entra si está logueado
+    canActivate: [authGuard]
   },
+
+  // ADMIN: Para el control total de PírituFood
   {
     path: 'admin',
     loadComponent: () => import('./features/admin/admin-panel.component/admin-panel.component').then(m => m.AdminPanelComponent),
-    canActivate: [authGuard] // Solo entra si está logueado
+    canActivate: [authGuard]
   },
-  // -------------------------
-  {
-    path: '',
-    redirectTo: 'store',
-    pathMatch: 'full'
-  },
+
+  // --- MANEJO DE ERRORES ---
+  
+  // Si ponen una ruta que no existe, los mandamos de vuelta al Lobby
   {
     path: '**',
-    redirectTo: 'store'
+    redirectTo: ''
   }
 ];
