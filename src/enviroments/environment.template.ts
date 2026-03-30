f89d12cfb9fac4c4177f67d38cea4813d5777bb9
Castropy <randrescastroi@gmail.com>
2026-03-29 23:17:47 -0400

    feat: setup firebase and documentation safely

---
+++ src/enviroments/environment.template.ts
@@ -0,0 +1,13 @@
+// Este es un archivo de plantilla. 
+// Copia este contenido en un archivo llamado 'environment.ts' y rellena los datos.
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "PON_TU_API_KEY_AQUI",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    projectId: "TU_PROYECTO_ID",
+    storageBucket: "TU_PROYECTO.appspot.com",
+    messagingSenderId: "TU_ID",
+    appId: "TU_APP_ID"
+  }
+};
